ad2c9d28d3ba43060280915ef6b4820725d607ef
Aravind <aravindparavind10@gmail.com>
2024-06-29 11:52:40 +0530

    routes

---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+const { login } = require("../controller/loginController")
+const { addData } = require("../controller/addDataController")
+const { creditData } = require("../controller/creditController")
+const { debitData } = require("../controller/debitDataController")
+const { getData } = require("../controller/getDataController")
+
+router.post('/login', login)
+
+router.post('/addData', addData)
+
+router.put('/creditData', creditData)
+
+router.put('/debitData', debitData)
+
+router.get('/getData', getData)
+
+module.exports = router

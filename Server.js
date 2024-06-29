96c294dd32e019cb951d27724a7e315f3fab2e62
amalsaj <amalsaji218@gmail.com>
2024-06-29 11:59:48 +0530

    fixed router problem

--- Server.js
+++ Server.js
@@ -3,21 +3,19 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./db/db");
-const cors = require("cors")
-const routes = require("./routes/user")
+const cors = require("cors");
+const routes = require("./routes/user");
 
 // Connect to MongoDB
 connectDB();
 
-// Set up body parser middleware
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 
 //cors
-app.use(cors())
+app.use(cors());
 
-app.use(routes)
+//routes defined
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
